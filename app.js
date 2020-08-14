5964644d6d678fc31375187018953194e016bb5c
Shrihari10 <62280755+Shrihari10@users.noreply.github.com>
2020-08-14 23:31:13 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const passport = require('passport');
 const path = require('path')
 const app = express();
 const ejs = require('ejs')
-const multer = require('multer');
+
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 
@@ -15,39 +15,12 @@ app.use(bodyParser.json());
 
 const db = require('./config/keys.js').MongoURI;
 // Public folder
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "Public")));
+app.use(express.static(path.join(__dirname, "Public/uploads")));
 
 require('./config/passport')(passport);
 
-//storage engine
-const storage = multer.diskStorage({
-    destination: './Public/uploads/',
-    filename: function(req, file, cb){
-        cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-});
-
-//Upload init
-const upload = multer ({
- storage: storage,
- fileFilter: function(req, file, cb){
-     checkFileType(file,cb);
- }
-}).single('myImage');
-
-function checkFileType(file,cb){
-    const filetypes = /jpeg|jpg|png|gif/;
-
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = filetypes.test(file.mimetype);
-
-    if( mimetype && extname){
-        return cb(null, true);
-        console.log('hi');
-    } else {
-        cb('err: not image');
-    }
-}
+app.use( require("./routes/posts.js"));
 
 mongoose.connect(db, { 
     authSource: 'admin',
@@ -59,7 +32,9 @@ mongoose.connect(db, {
 
 //middleware EJS
 app.use(expressLayouts);
+app.set('views', path.join(__dirname, "./template/views"));
 app.set('view engine', 'ejs');
+
 
 //Bodyparser
 app.use(express.urlencoded( {
@@ -90,28 +65,6 @@ app.use((req,res,next) => {
 app.use("/", require("./routes/index.js"));
 app.use("/users", require("./routes/users.js"));
 
-app.post('/upload', function(req, res) {
-    upload(req, res, (err) =>{
-        if(err) {
-            console.log('1');
-            res.render('views/dashboard.ejs', { msg: err});
-            
-        } else {
-            if( req.file == undefined){
-                console.log('2');
-                res.render('views/dashboard.ejs' , { msg: "error: no file selected"});
-               
-            }
-            else{
-                console.log('3');
-                res.render('views/dashboard.ejs', {
-                    msg: 'file uploaded',
-                    file: `./Public/uploads/${req.file.filename}`
-                });
-                
-            }
-        }
-    });
-});
+
 
 app.listen(PORT,console.log(`Running on PORT ${PORT}`));
